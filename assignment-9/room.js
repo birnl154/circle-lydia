d06f43ebc5c653b72f2ab6c42781e3f04d90318c
birnl154 <birnl154@newschool.edu>
2024-11-25 17:55:30 -0600

    file

---
+++ assignment-9/room.js
@@ -0,0 +1,8 @@
+button.addEventListener('click', function () {
+    if (colorDiv.style.backgroundColor === 'darkblue') {
+      colorDiv.style.backgroundColor = 'yellow';
+    } else {
+      colorDiv.style.backgroundColor = 'darkblue';
+    }
+  });
+  ``
