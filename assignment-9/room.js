5324ff73530bbdacf3aa64947193fa0df3fc0c13
birnl154 <birnl154@newschool.edu>
2024-11-26 11:51:59 -0600

    redo

--- assignment-9/room.js
+++ assignment-9/room.js
@@ -1,10 +1,9 @@
-// Select the button and the 'night' div
+
 const button = document.querySelector('button');
 const nightDiv = document.getElementById('night');
 
-// Add an event listener to the button
 button.addEventListener('click', function () {
-  // Toggle the 'day' and 'night' classes
+ 
   if (nightDiv.classList.contains('day')) {
     nightDiv.classList.remove('day');
     nightDiv.classList.add('night');
