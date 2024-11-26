c6743657237131c4a324670eded5dfe65e393bb9
birnl154 <birnl154@newschool.edu>
2024-11-25 20:01:40 -0600

    finish assignment

--- assignment-9/room.js
+++ assignment-9/room.js
@@ -1,8 +1,15 @@
+// Select the button and the 'night' div
+const button = document.querySelector('button');
+const nightDiv = document.getElementById('night');
+
+// Add an event listener to the button
 button.addEventListener('click', function () {
-    if (colorDiv.style.backgroundColor === 'darkblue') {
-      colorDiv.style.backgroundColor = 'yellow';
-    } else {
-      colorDiv.style.backgroundColor = 'darkblue';
-    }
-  });
-  ``
+  // Toggle the 'day' and 'night' classes
+  if (nightDiv.classList.contains('day')) {
+    nightDiv.classList.remove('day');
+    nightDiv.classList.add('night');
+  } else {
+    nightDiv.classList.remove('night');
+    nightDiv.classList.add('day');
+  }
+});
