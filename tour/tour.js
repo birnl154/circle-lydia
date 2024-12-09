f38da705515ef93fe82b1131238cab39f7f5b170
birnl154 <birnl154@newschool.edu>
2024-12-08 21:00:35 -0500

    done

---
+++ tour/tour.js
@@ -0,0 +1,14 @@
+
+const title = document.getElementById('title');
+
+title.addEventListener('mouseover', () => {
+    title.textContent = ' this website is a tour of all the films nominated for Best Picture Academy Award in the past ten years that did not win—aka, the Losers.';
+    title.classList.remove('hover-title');
+    title.classList.add('hover-paragraph');
+});
+
+title.addEventListener('mouseout', () => {
+    title.textContent = 'Losers';
+    title.classList.remove('hover-paragraph');
+    title.classList.add('hover-title');
+});
